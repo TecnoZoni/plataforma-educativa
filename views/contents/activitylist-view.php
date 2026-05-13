c1fb75d9d7e0dfcfe8ef1b5d4bf37b92ddb5d3f6
tecnozoni <agustinminzoni@gmail.com>
2026-05-13 16:42:11 -0300

    feat(activity): ordenar actividades sin corregir primero y agregar buscador por clase y alumno

--- views/contents/activitylist-view.php
+++ views/contents/activitylist-view.php
@@ -8,23 +8,66 @@
 	</p>
 </div>
 
-<?php 
+<?php
 	require_once "./controllers/activityController.php";
 
 	$insVideo = new activityController();
+
+	if(isset($_POST['search_init'])){
+		$_SESSION['activitySearch']=$_POST['search_init'];
+	}
+
+	if(isset($_POST['search_destroy'])){
+		unset($_SESSION['activitySearch']);
+	}
 ?>
+<div class="container-fluid">
+	<?php if(!isset($_SESSION['activitySearch']) || empty($_SESSION['activitySearch'])): ?>
+	<form action="" method="POST" enctype="multipart/form-data" autocomplete="off" class="well">
+		<div class="row">
+			<div class="col-xs-12 col-md-8 col-md-offset-2">
+				<div class="form-group label-floating">
+				  	<span class="control-label">Buscar actividad por título de clase o nombre de alumno</span>
+				  	<input class="form-control" type="text" name="search_init" required="">
+				</div>
+			</div>
+			<div class="col-xs-12">
+				<p class="text-center">
+			    	<button type="submit" class="btn btn-primary btn-raised btn-sm"><i class="zmdi zmdi-search"></i> &nbsp; Buscar</button>
+			    </p>
+			</div>
+		</div>
+	</form>
+	<?php else: ?>
+	<form action="" method="POST" enctype="multipart/form-data" autocomplete="off" class="well">
+		<p class="lead text-center">Su última búsqueda fue <strong>“<?php echo $_SESSION['activitySearch']; ?>”</strong></p>
+		<div class="row">
+			<input class="form-control" type="hidden" name="search_destroy" value="1">
+			<div class="col-xs-12">
+				<p class="text-center">
+			    	<button type="submit" class="btn btn-danger btn-raised btn-sm"><i class="zmdi zmdi-delete"></i> &nbsp; Eliminar búsqueda</button>
+			    </p>
+			</div>
+		</div>
+	</form>
+	<?php endif; ?>
+</div>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-xs-12">
 			<div class="panel panel-success">
 			  	<div class="panel-heading">
-			    	<h3 class="panel-title"><i class="zmdi zmdi-format-list-bulleted"></i> Lista de actividades</h3>
+			    	<h3 class="panel-title"><i class="zmdi zmdi-format-list-bulleted"></i> Lista de actividades<?php if(isset($_SESSION['activitySearch']) && !empty($_SESSION['activitySearch'])): ?> — búsqueda: "<?php echo $_SESSION['activitySearch']; ?>"<?php endif; ?></h3>
 			  	</div>
 			  	<div class="panel-body">
 					<div class="table-responsive">
 						<?php
 							$page=explode("/", $_GET['views']);
-							echo $insVideo->pagination_activity_controller($page[1],10);
+							if(isset($_SESSION['activitySearch']) && !empty($_SESSION['activitySearch'])){
+								echo $insVideo->pagination_activity_search_controller($page[1],10,$_SESSION['activitySearch']);
+							}else{
+								echo $insVideo->pagination_activity_controller($page[1],10);
+							}
 						?>
 					</div>
 			  	</div>
